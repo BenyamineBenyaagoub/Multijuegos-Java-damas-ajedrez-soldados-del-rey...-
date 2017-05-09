5b951bd01cb9c0d58721f0bdf744281c3061c30c
BenyamineBenyaagoub <ben_1995@hotmail.es>
2017-05-10 00:55:39 +0200

    Add files via upload

---
+++ Promociones.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeAjedrez;
+
+/**
+ *
+ * @author ibra
+ */
+public class Promociones {
+    //metodoo que preginta de que color es la pieza que hay arriba.
+    //Metodo para saber solor de pieza de abajo
+    //metodo que haga un Casting de peon a la pieza deseada
+    
+}
